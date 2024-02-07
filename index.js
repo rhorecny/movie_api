802f59f3204bc45fcb65ddbb417c31a957ecc879
Ryan Horecny <129782304+rhorecny@users.noreply.github.com>
2024-02-07 15:46:43 -0500

    Added endpoints and documentation
    Added endpoints to index.js to allow users to get movie information, director information, and genre information. Also added ability to add users, change user name, add favorite movies, remove favorite movies, and delete user by id.

--- index.js
+++ index.js
@@ -1,59 +1,157 @@
 const express = require("express"),
-  morgan = require("morgan");
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 const app = express();
 
-app.use(morgan("common"));
+app.use(bodyParser.json());
+
+//users
+
+let users = [
+  {
+    "id": "1",
+    "name": "John",
+    "favoriteMovie":[]
+    },
+  {
+    "id": "2",
+    "name": "Ryan",
+    "favoriteMovie": ["Star Wars: Episode VII - The Force Awakens", "Avatar"]
+  }
+]
 
 //top ten list
-let topMovies = [
+let movies = [
   {
-    title: "Movie 1",
-    director: "Director 1",
+    Id: "1",
+    Title: "Avatar",
+      Genre: { 
+      "Name": "Action",
+      "Description": "The action film is a film genre which predominantly feature chase sequences, fights, shootouts, explosions, and stunt work."
+    },
+    Director:{
+      "Name": "James Cameron"
+    },
+    ReleaseYear: "2009"
   },
 
   {
-    title: "Movie 2",
-    director: "Director 2",
+    Id: "2",
+    Title: "Avengers: Endgame",
+    Genre: { 
+      "Name": "Action",
+      "Description": "The action film is a film genre which predominantly feature chase sequences, fights, shootouts, explosions, and stunt work."
+    },
+    Director:{
+      "Name": "Anthony Russo"
+    },
+    ReleaseYear: "2019"
   },
 
   {
-    title: "Movie 3",
-    director: "Director 3",
+    Id: "3",
+    Title: "Avatar: The Way of Water",
+    Genre: { 
+      "Name": "Action",
+      "Description": "The action film is a film genre which predominantly feature chase sequences, fights, shootouts, explosions, and stunt work."
+    },
+    Director:{
+      "Name": "James Cameron"
+    },
+    ReleaseYear: "2022"
   },
 
   {
-    title: "Movie 4",
-    director: "Director 4",
+    Id: "4",
+    Title: "Titanic",
+    Genre: {
+      "Name": "Drama",
+      "Description": "In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
+    },
+    Director:{
+      "Name": "James Cameron"
+    },
+    ReleaseYear: "1997"
   },
 
   {
-    title: "Movie 5",
-    director: "Director 5",
+    Id: "5",
+    Title: "Star Wars: Episode VII - The Force Awakens",
+    Genre: { 
+      "Name": "Action",
+      "Description": "The action film is a film genre which predominantly feature chase sequences, fights, shootouts, explosions, and stunt work."
+    },
+    Director:{
+      "Name": "JJ Abrams"
+    },
+    ReleaseYear: "2015"
   },
 
   {
-    title: "Movie 6",
-    director: "Director 6",
+    Id: "6",
+    Title: "Avengers: Infinity War", 
+    Genre: { 
+      "Name": "Action",
+      "Description": "The action film is a film genre which predominantly feature chase sequences, fights, shootouts, explosions, and stunt work."
+    },
+    Director:{
+      "Name": "Anthony Russo"
+    },
+    ReleaseYear: "2018"
+    
   },
 
   {
-    title: "Movie 7",
-    director: "Director 7",
+    Id: "7",
+    Title: "Spider-Man: No Way Home",
+    Genre: { 
+      "Name": "Action",
+      "Description": "The action film is a film genre which predominantly feature chase sequences, fights, shootouts, explosions, and stunt work."
+    },
+    Director:{
+      "Name": "Jon Watts"
+    },
+    ReleaseYear: "2021"
   },
 
   {
-    title: "Movie 8",
-    director: "Director 8",
+    Id: "8",
+    Title: "Jurassic World",
+    Genre: { 
+      Name: "Action",
+      Description: "The action film is a film genre which predominantly feature chase sequences, fights, shootouts, explosions, and stunt work."
+    },
+    Director:{
+      "Name":"Colin Trevorrow"
+    },
+    ReleaseYear: "2015"
+
   },
 
   {
-    title: "Movie 9",
-    director: "Director 9",
+    Id: "9",
+    Title: "The Lion King",
+    Genre:{ 
+      Name:  "Animation",
+      Description: "Animation is a filmmaking technique by which still images are manipulated to create moving images."
+    },
+    Director:{
+      "Name": "Jon Favreau"
+    },
+    ReleaseYear: "2019"
   },
 
   {
-    title: "Movie 10",
-    director: "Director 10",
+    Id: "10",
+    Title: "Avengers: Age of Ultron", 
+    Genre: { 
+      Name: "Action",
+      Description: "The action film is a film genre which predominantly feature chase sequences, fights, shootouts, explosions, and stunt work."
+    },
+    Director:{
+      "Name":"Joss Whedon"
+    },
+    ReleaseYear: "2012"
   },
 ];
 
@@ -71,8 +169,116 @@ app.get("/", (req, res) => {
 });
 
 app.get("/movies", (req, res) => {
-  res.json(topMovies);
+  res.status(200).json(movies);
 });
+
+app.get("/movies/:title", (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find((movie) => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("No such movie in database!");
+  }
+});
+
+app.get('/movies/genre/:genreName', (req, res) => {
+const { genreName } = req.params;
+const genre = movies.find( movie => movie.Genre.Name === genreName).Genre;
+
+if (genre) {
+  res.status(200).json(genre);
+} else {
+  res.status(400).send("This Genre doesn't exist in this database!")
+}
+});
+
+app.get('/movies/director/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find( movie => movie.Director.Name === directorName).Director;
+  
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("This Genre doesn't exist in this database!")
+  }
+  });
+  
+
+//post requests
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name){
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send('Users need names.')
+  }
+
+});
+
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id);
+
+  if (user){
+    user.favoriteMovie.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send('no such user');
+  }
+});
+
+
+//delete requests
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id);
+
+  if (user){
+    user.favoriteMovie = user.favoriteMovie.filter( title => title !== movieTitle)
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send('no such user');
+  }
+});
+
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find( user => user.id == id);
+
+  if (user){
+    users = users.filter( user => user.id != id);
+    res.status(200).send(`user ${id} has been deleted.`);
+  } else {
+    res.status(400).send('no such user');
+  }
+});
+
+
+
+//put requests
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == id)
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('no such user.')
+  }
+
+});
+
 //static
 app.use(
   "/documentation",
